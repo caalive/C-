0a83e64c9429c1e76fbacfdae34272e621d43057
caalive <caa502@live.com>
2017-11-19 09:57:04 +0800

    added some example about C++

---
+++ C++/exercise6.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: exercise6.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 18 Nov 2017 09:48:34 PM CST
+ ************************************************************************/
+#include <iostream>
+using namespace std;
+
+int DemoConsoleOutput()
+{
+    cout << "This is a simple string literal" << endl;
+    cout << "Writing number five: " << 5 << endl;
+    cout << "Performing division 10 / 5 = " << 10 / 5 << endl;
+    cout << "Pi when approximated is 22 / 7 = " << 22 / 7 << endl;
+    cout << "Pi actually is 22 / 7 = " << 22.0 / 7 << endl;
+    return 0;
+}
+
+int main()
+{
+    return DemoConsoleOutput();
+}
